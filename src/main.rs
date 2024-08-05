3f74ec8792cecb5559f62421b5ec46e85ec5b790
McFlanky <cturjanica02@gmail.com>
2024-08-04 21:01:08 -0400

    Changed window name, added 2d camera, spawning 2048 board, used color to change look of board, changed background color, folled out board tile placeholders with math, defined associated functions on structs to abstract math logic away

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
+use bevy::prelude::*;
+use itertools::Itertools;
+
+mod colors;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::hex("1f2638").unwrap()))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "2048".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_startup_systems((setup, spawn_board))
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+const TILE_SIZE: f32 = 40.0;
+const TILE_SPACER: f32 = 10.0;
+
+#[derive(Component)]
+struct Board {
+    size: u8,
+    physical_size: f32,
+}
+
+impl Board {
+    fn new(size: u8) -> Self {
+        let physical_size = f32::from(size) * TILE_SIZE + f32::from(size + 1) * TILE_SPACER;
+        Board {
+            size,
+            physical_size,
+        }
+    }
+    fn cell_position_to_physical(&self, pos: u8) -> f32 {
+        let offset = -self.physical_size / 2.0 + 0.5 * TILE_SIZE;
+
+        offset + f32::from(pos) * TILE_SIZE + f32::from(pos + 1) * TILE_SPACER
+    }
+}
+
+fn spawn_board(mut commands: Commands) {
+    let board = Board::new(4);
+
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: colors::BOARD,
+                custom_size: Some(Vec2::new(board.physical_size, board.physical_size)),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|builder| {
+            for tile in (0..board.size).cartesian_product(0..board.size) {
+                builder.spawn(SpriteBundle {
+                    sprite: Sprite {
+                        color: colors::TILE_PLACEHOLDER,
+                        custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                        ..default()
+                    },
+                    transform: Transform::from_xyz(
+                        board.cell_position_to_physical(tile.0),
+                        board.cell_position_to_physical(tile.1),
+                        1.0,
+                    ),
+                    ..default()
+                });
+            }
+        })
+        .insert(board);
 }
